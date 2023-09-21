1d1ad9777c1eb7cab415395ee1c15317cc7b7da3
Laye <tlayefaah@gmail.com>
2023-09-21 16:07:47 +0100

    reward

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -67,18 +67,6 @@ export default function Hero() {
               // onClick={handleImageClick}
               className=" object-center h-full bg-reen-900 w-full  object-contain"
             />
-            <div className="bg-red-00 mix-blend-hard-light bg-blend-hard-light absolute lg:hidden  z-10  bottom-20 -right-10 ">
-              <div className=" lg:block opacity-[100%] mix-blend-hard-light bg-blend-hard-light">
-                <Image
-                  src={glow}
-                  width={500}
-                  height={500}
-                  alt="glow"
-                  // onClick={handleImageClick}
-                  className=" object-center w-[350px] lg:w-[600px] 2xl:w-[800px]"
-                />
-              </div>
-            </div>
           </div>
           <div
             data-aos="zoom-out"
@@ -143,7 +131,18 @@ export default function Hero() {
       <div className="bg-red-00 mix-blend-hard-light bg-blend-hard-light absolute hidden lg:block  z-10  -top-12 -right-32 ">
         <Glow />
       </div>
-
+      <div className="bg-red-00 mix-blend-hard-light bg-blend-hard-light absolute lg:hidden  z-10  bottom-20 -right-10 ">
+        <div className=" lg:block opacity-[100%] mix-blend-hard-light bg-blend-hard-light">
+          <Image
+            src={glow}
+            width={500}
+            height={500}
+            alt="glow"
+            // onClick={handleImageClick}
+            className=" object-center w-[350px] lg:w-[600px] 2xl:w-[800px]"
+          />
+        </div>
+      </div>
       <div className=" absolute  mix-blend-hard-light bg-blend-hard-light -z-10 lg:hidden  top-4 -left-14 ">
         <div className=" lg:block opacity-[%] mix-blend-hard-light bg-blend-hard-light">
           <Image
