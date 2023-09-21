d82c10d15ed26078c8571a05aea33c3a37b6bb05
Laye <tlayefaah@gmail.com>
2023-09-21 16:11:36 +0100

    reward

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -131,7 +131,7 @@ export default function Hero() {
       <div className="bg-red-00 mix-blend-hard-light bg-blend-hard-light absolute hidden lg:block  z-10  -top-12 -right-32 ">
         <Glow />
       </div>
-      <div className="bg-red-00 mix-blend-hard-light bg-blend-hard-light absolute lg:hidden  z-10  bottom-28 -right-10 ">
+      <div className="bg-red-00 mix-blend-hard-light bg-blend-hard-light absolute lg:hidden  z-10  bottom-20 -right-10 ">
         <Glow />
       </div>
       <div className=" absolute  mix-blend-hard-light bg-blend-hard-light -z-10 lg:hidden  top-4 -left-14 ">
