d47bb02249df763fdcf56ba9ab8e4127e6d4e507
Laye <tlayefaah@gmail.com>
2023-09-23 15:35:42 +0100

    medium screen

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -60,7 +60,7 @@ export default function Hero() {
             }}
             // data-aos="zoom-in"
             data-aos-delay="50"
-            className="lg:hidden h-full bg-blu-300 w-full  flex  justify-center place-self-center mx-auto md:h-1/2 md:w-1/2"
+            className="lg:hidden h-full bg-blu-300 w-full  flex  justify-center place-self-center mx-auto "
           >
             <Image
               src={hackGuy}
@@ -74,7 +74,7 @@ export default function Hero() {
           <div
             data-aos="zoom-in"
             data-aos-delay="500"
-            className=" mix-blend-hard-light slow-spin h-[150%] w-[150%] bg-blue300 absolute  z-40  -right-[29%] -top-[34%] md:right-20 lg:hidden flex justify-end bg-geen-300"
+            className=" mix-blend-hard-light slow-spin h-[150%] w-[150%] bg-blue300 absolute  z-40  -right-[29%] -top-[34%] md:-top-[25%] lg:hidden flex justify-end bg-geen-300"
           >
             <Image
               src={globe}
